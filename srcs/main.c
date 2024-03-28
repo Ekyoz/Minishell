8d56263c40bf32162d4dd823c7c1cc3c45c59e92
Bastien Poyet <bpoyet@z1r2p3.42lyon.fr>
2024-03-28 11:53:50 +0100

    rien

--- srcs/main.c
+++ srcs/main.c
@@ -12,6 +12,17 @@
 
 #include "../include/minishell.h"
 
+void change_directory(char *input)
+{
+    char buffer[1024];
+
+    getcwd(buffer, sizeof(buffer));
+    if(chdir(input) == -1)
+        perror("chdir");
+    getcwd(buffer, sizeof(buffer));
+    printf("%s\n", buffer);
+}
+
 int main()
 {
     char *input;
@@ -20,7 +31,8 @@ int main()
     {
         input = readline("Minishell >");
         add_history(input);
-        if(ft_strncmp(input, "exit", 4))
+        change_directory(input);
+        if(!ft_strncmp(input, "exit", 5))
             break;
     }
     return 0;
