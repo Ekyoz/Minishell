00dfc42989f47e3f3e514e364a3e0c9e37d342fc
Bastien Poyet <bpoyet@z1r2p2.42lyon.fr>
2024-03-28 09:53:22 +0100

    modification du main

--- include/minishell.h
+++ include/minishell.h
@@ -13,8 +13,8 @@
 #ifndef MINISHELL_H
 #define MINISHELL_H
 
-# include "libft.h"
-# include "get_next_line.h"
+# include "LibFT/include/libft.h"
+//# include "get_next_line.h"
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdio.h>
--- srcs/main.c
+++ srcs/main.c
@@ -3,15 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: atresall <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: bpoyet <bpoyet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 12:42:21 by atresall          #+#    #+#             */
-/*   Updated: 2024/03/26 12:42:21 by atresall         ###   ########.fr       */
+/*   Updated: 2024/03/28 09:38:27 by bpoyet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "../include/minishell.h"
 
-int main() {
+int main()
+{
+    char *input;
+
+    while (true)
+    {
+        input = readline("Minishell >");
+        add_history(input);
+        if(ft_strncmp(input, "exit", 4))
+            break;
+    }
     return 0;
 }
