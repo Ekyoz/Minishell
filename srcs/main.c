f4ad8459b51edf82ad5849808f52170e732d42a7
Alexandre <alexandre.tresallet@gmail.com>
2024-03-28 11:54:43 +0100

    Changes by bpoyet

--- srcs/main.c
+++ srcs/main.c
@@ -6,12 +6,19 @@
 /*   By: atresall <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 12:42:21 by atresall          #+#    #+#             */
-/*   Updated: 2024/03/26 12:42:21 by atresall         ###   ########.fr       */
+/*   Updated: 2024/03/26 17:26:53 by atresall         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 int main() {
+	char *input;
+	while (1) {
+		input = readline("Minishell: $");
+		add_history(input);
+		if (!ft_strncmp(input, "exit", 5))
+			break;
+	}
     return 0;
 }
