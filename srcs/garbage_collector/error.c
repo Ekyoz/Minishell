64bcb353e8a5317fb3e835e1031ca9befa4349d2
bastpoy <bastien.poyet@outlook.fr>
2024-05-07 17:45:35 +0200

    push

--- srcs/garbage_collector/error.c
+++ srcs/garbage_collector/error.c
@@ -6,7 +6,7 @@
 /*   By: bpoyet <bpoyet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 16:43:01 by bpoyet            #+#    #+#             */
-/*   Updated: 2024/05/07 17:36:16 by bpoyet           ###   ########.fr       */
+/*   Updated: 2024/05/07 17:44:34 by bpoyet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,18 @@
 
 void print_error(int errorcode, t_node *node, t_tree *tree)
 {
-    if(errorcode = 1 && tree->error[0] == 0) // command not found
+    if(errorcode = 1 && tree->error[1] == 1) // command not found
     {
         Printf("Command not found: %s\n", node->args[0]);
+        exit(errno);
+    }
+}
+
+void check_error_code(t_tree *tree)
+{
+    if(tree->error[0] == 1) // erreur d'ouverture de fichiers
+    {
+        perror("");
         exit(errno);
     }
 }
