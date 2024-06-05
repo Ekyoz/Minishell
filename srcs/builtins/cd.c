f700e50174a641e632a9f0b5e82ba7c4b6dbff3b
Alexandre <alexandre.tresallet@gmail.com>
2024-06-05 15:34:17 +0200

    ajout de cd.c

---
+++ srcs/builtins/cd.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cd.c                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bastpoy <bastpoy@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/26 16:30:34 by atresall          #+#    #+#             */
+/*   Updated: 2024/05/29 12:00:15 by bastpoy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static int test_directory(char * path)
+{
+    struct stat	st;
+
+    ft_putstr_fd("minishell: cd: ", 2);
+    ft_putstr_fd(path, 2);
+    signal_status = 1;
+    if (stat(path, &st) == -1)
+    {
+        ft_putstr_fd(": No such file or directory", 2);
+        signal_status = 127;
+    }
+    else if (!(st.st_mode & S_IXUSR))
+        ft_putstr_fd(": Permission denied", 2);
+    else
+        ft_putstr_fd(": Not a directory", 2);
+    ft_putchar_fd('\n', 2);
+    return (1);
+}
+
+static int change_dir(t_tree *tree, t_node *node, char *path)
+{
+    char cwd[1024];
+
+    if(!chdir(path))
+    {
+        getcwd(cwd, sizeof(cwd));
+        set_env(tree, tree->env, "PWD=", cwd);
+        return(1);
+    }
+    else
+        test_directory(node->args[1]);
+    return (0);
+}
+
+static void tild(t_tree *tree, t_node *node, char* homepath)
+{
+    char *path;
+
+    homepath = get_env(tree->env, "HOME="); // me renvoie le path de l'home
+    if(!homepath)
+    {
+        ft_putstr_fd("cd: HOME not set\n", 2);
+        signal_status = 1;
+        return;
+    }
+    path = ft_substr(node->args[1], 1, ft_strlen(node->args[1]) - 1); // je decalle de un pour enlever le tild
+    path = ft_strjoin(homepath, path);
+    if (change_dir(tree, node, path))
+        set_env(tree, tree->env, "PWD=", path);
+}
+
+static int cd_alone(t_tree *tree, t_node *node)
+{
+    char *homepath;
+
+    homepath = get_env(tree->env, "HOME=");
+    if(!homepath) // je vais dans le path de HOME
+    {
+        ft_putstr_fd("cd: HOME not set\n", 2);
+        signal_status = 1;
+    }
+    change_dir(tree, node, homepath);
+    free(homepath);
+    return(0);
+}
+
+int do_cd(t_tree *tree, t_node *node)
+{
+    char *homepath;
+
+    homepath = NULL;
+    if(node->args && node->args[1] && node->args[2])
+    {
+        ft_putstr_fd("cd: too many arguments\n", 2);
+        err_free_all(tree);
+    }
+    else if(!node->args[1]) // dans le cas de cd tout seul
+        cd_alone(tree, node);
+    else if(node->args && !ft_strncmp(node->args[1], "-", 1)) // tiret
+    {
+        ft_putstr_fd("argument not supported\n", 2);
+        signal_status = 1;
+    }
+    else if(node->args && !ft_strncmp(node->args[1], "~", 1)) // tild
+        tild(tree, node, homepath);
+    else // cd commande normal
+        change_dir(tree, node, node->args[1]);
+    return(1);
+}
