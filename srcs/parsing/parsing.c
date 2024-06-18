2765f563cf7ae324c3757d464cf39cc6f0101520
alexandre <alexandre.tresallet@gmail.com>
2024-06-18 14:14:04 +0200

    leak fix in expand

--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -62,7 +62,8 @@ static void	parsing_redir(t_token **head, char **c_pipe, char **c_splitted,
 		append_token(head, TOKEN_WORD, c_cmd);
 		append_token_redir(c_redirs, c_splitted, head);
 	}
-	free_token(c_splitted, c_redirs, c_cmd, NULL);
+	free_array(&c_splitted);
+	free_token(NULL, c_redirs, c_cmd, NULL);
 }
 
 static void	append_token_redir(char **c_redirs, char **c_splitted,
